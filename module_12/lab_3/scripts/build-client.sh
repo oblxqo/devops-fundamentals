9a46c2888f8a7ab5590c0934257dc82b1f61263d
Anton Lyashkevich <anton_lyashkevich@epam.com>
2023-12-28 13:09:25 +0300

    feat: module-12/lab-3(Infrastructure-as-Code-CI/CD-fundamentals):
    - Sub-task 1 - Configure Continuous Integrations (CI) for local development: front-end.
    - Sub-task 2 - Configure CI/CD pipeline for front-end app
    - Sub-task 3 - Configure CI/CD pipeline for back-end app

---
+++ module_12/lab_3/scripts/build-client.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+# Navigate to the Angular application directory from ROOT_DIR/scripts folder
+SCRIPT_DIR=`dirname "$(readlink -f "$BASH_SOURCE")"`
+ROOT_DIR=${SCRIPT_DIR%/*}
+clientBuildFile=$ROOT_DIR/dist/client-app.tar.gz
+
+# Load environment variables
+export $(grep -v '^#' $ROOT_DIR/.env | xargs -d '\n')
+
+echo "SCRIPT_DIR $SCRIPT_DIR"
+echo "ROOT_DIR $ROOT_DIR"
+
+# Check if the client-app.zip exists
+if [ -e "$clientBuildFile" ]; then
+  echo "$clientBuildFile already exists, removing the file..."
+  rm $clientBuildFile
+  echo "$clientBuildFile was removed."
+fi
+
+# Install the app's npm dependencies
+echo "Installing npm dependencies..."
+cd $ROOT_DIR
+npm ci
+
+# Invoke the client app's build command with --configuration flag
+echo "Building client app with configuration: $ENV_CONFIGURATION..."
+npm run build --configuration="$ENV_CONFIGURATION"
+
+# Compress all built content/files in one client-app.zip file
+echo "Compressing built files into client-app.zip..."
+tar --gzip --xattrs -cpf $clientBuildFile $ROOT_DIR/dist/
+
+# Count the number of files in the client app's dist folder
+echo "Counting the number of files in the dist folder..."
+file_count=$(find $ROOT_DIR/dist/ -type f | wc -l)
+echo "There are $file_count files in the dist folder."
+
+echo "Build finished!"
